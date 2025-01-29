2c5b0ad50dce1dfdce31922e76ee2241e3e8d2c7
TalDebi <taldebi2@gmail.com>
2025-01-29 18:22:14 +0000

    added tests
    Signed-off-by: TalDebi <taldebi2@gmail.com>

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/TalDebi/namespacelabel
 go 1.22.0
 
 require (
+	github.com/go-logr/logr v1.4.2
 	github.com/onsi/ginkgo/v2 v2.19.0
 	github.com/onsi/gomega v1.33.1
 	k8s.io/api v0.31.0
@@ -24,7 +25,6 @@ require (
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
-	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-logr/zapr v1.3.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
@@ -82,6 +82,7 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240701130421-f6361c86f094 // indirect
 	google.golang.org/grpc v1.65.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/evanphx/json-patch.v4 v4.12.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- internal/webhook/v1alpha1/namespacelabel_webhook_test.go
+++ internal/webhook/v1alpha1/namespacelabel_webhook_test.go
@@ -17,55 +17,84 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"context"
+	"fmt"
+	"github.com/TalDebi/namespacelabel/internal"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"time"
 
 	danav1alpha1 "github.com/TalDebi/namespacelabel/api/v1alpha1"
-	// TODO (user): Add any additional imports if needed
 )
 
 var _ = Describe("NamespaceLabel Webhook", func() {
 	var (
 		obj       *danav1alpha1.NamespaceLabel
-		oldObj    *danav1alpha1.NamespaceLabel
 		validator NamespaceLabelCustomValidator
+		ctx       context.Context
+		scheme    *runtime.Scheme
 	)
 
+	testLabels := map[string]string{"team": "devops"}
+
 	BeforeEach(func() {
-		obj = &danav1alpha1.NamespaceLabel{}
-		oldObj = &danav1alpha1.NamespaceLabel{}
-		validator = NamespaceLabelCustomValidator{}
+		ctx = context.Background()
+		scheme = runtime.NewScheme()
+		Expect(danav1alpha1.AddToScheme(scheme)).To(Succeed())
+		resourceName := fmt.Sprintf("test-resource-%d", time.Now().UnixNano())
+		testNamespace := fmt.Sprintf("test-ns-%d", time.Now().UnixNano())
+		obj = &danav1alpha1.NamespaceLabel{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      resourceName,
+				Namespace: testNamespace,
+			},
+			Spec: danav1alpha1.NamespaceLabelSpec{
+				Labels: testLabels,
+			},
+		}
+		fakeClient := fake.NewClientBuilder().WithScheme(scheme).Build()
+		validator = NamespaceLabelCustomValidator{Client: fakeClient}
 		Expect(validator).NotTo(BeNil(), "Expected validator to be initialized")
-		Expect(oldObj).NotTo(BeNil(), "Expected oldObj to be initialized")
 		Expect(obj).NotTo(BeNil(), "Expected obj to be initialized")
-		// TODO (user): Add any setup logic common to all tests
 	})
 
-	AfterEach(func() {
-		// TODO (user): Add any teardown logic common to all tests
+	Context("When creating NamespaceLabel", func() {
+		It("Should allow creation if no existing NamespaceLabel is in the namespace", func() {
+			_, err := validator.ValidateCreate(ctx, obj)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("Should reject creation if a NamespaceLabel already exists in the namespace", func() {
+			validator.Client = fake.NewClientBuilder().WithScheme(scheme).WithObjects(obj).Build()
+
+			_, err := validator.ValidateCreate(ctx, obj)
+			Expect(err).To(HaveOccurred())
+		})
+
+		It("Should reject creation if a management label is used", func() {
+			obj.Spec.Labels[internal.ManagementLabelPrefix] = "test"
+			_, err := validator.ValidateCreate(ctx, obj)
+			Expect(err).To(HaveOccurred())
+		})
 	})
 
-	Context("When creating or updating NamespaceLabel under Validating Webhook", func() {
-		// TODO (user): Add logic for validating webhooks
-		// Example:
-		// It("Should deny creation if a required field is missing", func() {
-		//     By("simulating an invalid creation scenario")
-		//     obj.SomeRequiredField = ""
-		//     Expect(validator.ValidateCreate(ctx, obj)).Error().To(HaveOccurred())
-		// })
-		//
-		// It("Should admit creation if all required fields are present", func() {
-		//     By("simulating an invalid creation scenario")
-		//     obj.SomeRequiredField = "valid_value"
-		//     Expect(validator.ValidateCreate(ctx, obj)).To(BeNil())
-		// })
-		//
-		// It("Should validate updates correctly", func() {
-		//     By("simulating a valid update scenario")
-		//     oldObj.SomeRequiredField = "updated_value"
-		//     obj.SomeRequiredField = "updated_value"
-		//     Expect(validator.ValidateUpdate(ctx, oldObj, obj)).To(BeNil())
-		// })
+	Context("When updating NamespaceLabel", func() {
+		It("Should allow updating when labels are valid", func() {
+			newObj := obj.DeepCopy()
+			newObj.Spec.Labels = testLabels
+			_, err := validator.ValidateUpdate(ctx, obj, newObj)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("Should reject update if it introduces a management label", func() {
+			existingObj := obj.DeepCopy()
+			obj.Spec.Labels[internal.ManagementLabelPrefix] = "test"
+			_, err := validator.ValidateUpdate(ctx, existingObj, obj)
+			Expect(err).To(HaveOccurred())
+		})
 	})
 
 })
